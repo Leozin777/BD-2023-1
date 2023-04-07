242fc4e7def577c201681116010e28f6c2403387
leozin777 <leo.cardoso@rede.ulbra.br>
2023-04-06 23:09:05 -0300

    tde

--- semi/semi.sql
+++ semi/semi.sql
@@ -7,7 +7,7 @@ vendas_livros(id, id_venda, id_livro, qtd, valor_unit)
 
 -- 1 Crie uma view chamada "livros_mais_vendidos" que exiba o título, autor, preço e a quantidade de vezes que cada livro foi vendido.
 CREATE VIEW livros_mais_vendidos AS
-SELECT Livros.titulo as titulo, Autores.nome as nome_autor, Livros.valor_unit as preco, COUNT(vendas_livros.id_livro) as qtd_livro_vendido
+SELECT Livros.titulo as titulo, Autores.nome as nome_autor, Livros.valor_unit as preco, SUM(vendas_livros.qtd_livro_vendido) as qtd_livro_vendido
 FROM Livros
     JOIN Autores_livros
         on Livros.id = Autores_livros.id_livro
@@ -15,8 +15,8 @@ FROM Livros
         ON Autores.id = Autores_livros.id_autor
     JOIN vendas_livros
         ON vendas_livros.id_livro = Livros.id
-GROUP BY Livros.nome
---dúvidas sobre este group by"
+GROUP BY Livros.id
+ORDER BY qtd_livro_vendido DESC
 
 --2 Crie uma view que lista os autores que nunca venderam livros.
 CREATE VIEW autores_nunca_venderam AS
@@ -33,7 +33,7 @@ WHERE vendas_livros.id_livro IS NULL
 --3 Use a sua criatividade e crie uma view que se aplique nessa modelagem.
 --Clientes que mais compraram
 CREATE VIEW clientes__que_mais_compraram AS
-SELECT Clientes.nome as nome_cliente, COUNT(Vendas.id_cliente) as qtd_compras
+SELECT Clientes.nome as nome_cliente, SUM(Vendas.qtd_livro_vendido) as qtd_compras
 FROM Clientes
     JOIN Vendas
         ON Clientes.id = Vendas.id_cliente
---
+++ tde-07-04/tde.sql
@@ -0,0 +1,81 @@
+/*Escreva uma SP que receba, como parâmetro, o CPF de um autor e retorne a quantidade de livros escrito pelo mesmo.*/
+drop procedure qtd_livros_escritos_pelo_autor;
+delimiter $$
+
+create procedure qtd_livros_escritos_pelo_autor(p_cpf varchar(14))
+begin
+     select Autores.nome as nome_autor , count(Autores_livros.id_livro) as qtd_livros
+     from Autores_livros
+         join Autores
+             on Autores_livros.id_autor = Autores.id
+     where Autores.cpf = p_cpf;
+end $$
+delimiter ;
+
+call qtd_livros_escritos_pelo_autor('097.362.560-06');
+
+
+/*Crie uma SP que receba, como um parâmetro, a data de publicação de um livro e seu código.
+  O procedimento deve atualizar a tabela de livros, especificando a data de lançamento para o livro em questão.*/
+
+delimiter $$
+
+create procedure atualiza_data_lacamento_por_parametro(p_data_publicacao date, p_cod int)
+begin
+    update Livros set data_publicao = p_data_publicacao where id = p_cod;
+end $$
+
+delimiter ;
+
+/*
+Em algumas situações, SPs são utilizados para a manutenção da segurança do banco de dados. Nestes casos, realizamos inclusões, alterações e exclusões de dados, através de SPs. Crie SPs que recebem os parâmetros adequados e realizam as seguintes operações:
+
+a) Inserir uma linha na tabela de livros
+b) Excluir uma linha da tabela de livros
+c) Atualizar valores na tabela de livros
+*/
+
+--a:
+delimiter $$
+
+create procedure add_linha_tabela_livros(p_titulo varchar(50), p_valor_unit double, p_data_publicacao date)
+begin
+    insert into Livros(titulo, valor_unit, data_publicao) values(p_titulo, p_valor_unit, p_data_publicacao)
+end $$
+
+delimiter ;
+
+--b:
+delimiter $$
+
+create procedure deleta_linha_tabela_livros(p_cod int)
+begin
+    delete from Livros where id = p_cod;
+end $$
+
+delimiter ;
+
+--c:
+
+delimiter $$
+
+create procedure atualiza_linha_tabela_livros(p_cod int, p_valor_unit double = null, p_data_publicacao date = null)
+begin
+    if p_valor_unit != null and p_data_publicacao != null
+    begin
+        update Livros set valor_unit = p_valor_unit, data_publicao = p_data_publicacao where id = p_cod;
+    end
+
+    if p_valor_unit != null
+    begin
+        update Livros set valor_unit = p_valor_unit where id = p_cod;
+    end
+
+    if p_data_publicacao != null
+    begin 
+        update Livros set data_publicao = p_data_publicacao where id = p_cod;
+    end
+
+end $$
+
+delimiter ;
